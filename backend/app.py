87f93310e28d207d243d5b97ee59a82de3d5d66c
Christos Gkovaris <147056529+ChristosGkovarisCSE52@users.noreply.github.com>
2026-07-30 22:00:17 +0300

    feat: add logs API endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from routes.health import health_bp
 from routes.docs import docs_bp
+from routes.logs import logs_bp
 
 
 app = Flask(__name__)
 
 app.register_blueprint(health_bp)
 app.register_blueprint(docs_bp)
+app.register_blueprint(logs_bp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
